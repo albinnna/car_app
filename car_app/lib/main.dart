b2c00729d936ddaaae34aeef1031f84f1ecfa985
albinnna <abdilashymova.a@gmail.com>
2025-01-15 17:10:07 +0600

    feat/add welcome screen

---
+++ car_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:car_app/features/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CarApp());
+}
+
+class CarApp extends StatelessWidget {
+  const CarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WelcomeScreen(),
+    );
+  }
+}
---
+++ car_app/lib/theme/app_colors.dart
@@ -0,0 +1,5 @@
+abstract class AppColors {
+  static String hexColorTx = '#F6F6F6';
+  static const String colorBtn = 'C64949';
+  static Color colorBg = const Color(0xFFC64949);
+}
